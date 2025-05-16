6f32f87c8951e68589b478c680c3dc14d3352810
Taein5415 <kskk5050@naver.com>
2025-05-16 23:29:01 +0900

    [feat #1] @RequestBody 추가

--- NBTI/src/main/java/com/dao/nbti/user/application/controller/UserController.java
+++ NBTI/src/main/java/com/dao/nbti/user/application/controller/UserController.java
@@ -3,18 +3,21 @@ package com.dao.nbti.user.application.controller;
 import com.dao.nbti.common.dto.ApiResponse;
 import com.dao.nbti.user.application.dto.UserCreateRequest;
 import com.dao.nbti.user.application.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
     @GetMapping("/signup")
-    public ResponseEntity<ApiResponse<Void>> signup(UserCreateRequest userCreateRequest){
+    public ResponseEntity<ApiResponse<Void>> signup(@RequestBody UserCreateRequest userCreateRequest){
         userService.createUser(userCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(null));
     }
