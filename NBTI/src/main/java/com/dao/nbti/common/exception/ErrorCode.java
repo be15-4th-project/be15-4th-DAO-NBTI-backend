b8039b85330b880eff768ee621194bf902251ca8
Taein5415 <kskk5050@naver.com>
2025-05-16 23:30:16 +0900

    [feat #1] Exception 추가 및 UserService.createUser 구현

--- NBTI/src/main/java/com/dao/nbti/common/exception/ErrorCode.java
+++ NBTI/src/main/java/com/dao/nbti/common/exception/ErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PACKAGE)
 public enum ErrorCode {
-
+    //회원 오류 (00000 ~ 09999)
+    LOGIN_ID_ALREADY_EXISTS("10001", "이미 사용중인 ID 입니다.", HttpStatus.CONFLICT),
 
     // 공통 오류
     VALIDATION_ERROR("90001", "입력 값 검증 오류입니다.", HttpStatus.BAD_REQUEST),
--- NBTI/src/main/java/com/dao/nbti/user/application/service/UserService.java
+++ NBTI/src/main/java/com/dao/nbti/user/application/service/UserService.java
@@ -2,12 +2,31 @@ package com.dao.nbti.user.application.service;
 
 
 
+import com.dao.nbti.common.exception.ErrorCode;
 import com.dao.nbti.user.application.dto.UserCreateRequest;
+import com.dao.nbti.user.domain.aggregate.User;
+import com.dao.nbti.user.domain.repository.UserRepository;
+import com.dao.nbti.user.exception.UserException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    private final ModelMapper modelMapper;
+    private final PasswordEncoder passwordEncoder;
     public void createUser(UserCreateRequest userCreateRequest) {
-
+        log.info(userCreateRequest.toString());
+        User user = modelMapper.map(userCreateRequest, User.class);
+        if(userRepository.findByAccountId(user.getAccountId()).isPresent()){
+            throw new UserException(ErrorCode.LOGIN_ID_ALREADY_EXISTS);
+        }
+        user.setEncodedPassword(passwordEncoder.encode(userCreateRequest.getPassword()));
+        userRepository.save(user);
     }
 }
