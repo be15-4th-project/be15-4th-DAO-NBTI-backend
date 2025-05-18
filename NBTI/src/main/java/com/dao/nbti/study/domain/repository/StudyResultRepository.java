07ba4ddd9a59d579ea6fdefc4edcaea67b687c68
splguyjr <splguyjr@naver.com>
2025-05-18 17:25:45 +0900

    [feat] 학습, 학습 결과 레포지토리 생성

---
+++ NBTI/src/main/java/com/dao/nbti/study/domain/repository/StudyResultRepository.java
@@ -0,0 +1,7 @@
+package com.dao.nbti.study.domain.repository;
+
+import com.dao.nbti.study.domain.aggregate.StudyResult;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudyResultRepository extends JpaRepository<StudyResult, Integer> {
+}
