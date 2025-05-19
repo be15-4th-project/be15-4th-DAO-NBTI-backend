25039f3147fbaa607e87276ba0133ca4123ddbd8
Taein5415 <kskk5050@naver.com>
2025-05-19 22:05:26 +0900

    [feat #41] UserController.withdraw 메소드 및 예외처리 구현

--- NBTI/src/main/java/com/dao/nbti/user/application/controller/UserController.java
+++ NBTI/src/main/java/com/dao/nbti/user/application/controller/UserController.java
@@ -1,24 +1,45 @@
 package com.dao.nbti.user.application.controller;
 
 import com.dao.nbti.common.dto.ApiResponse;
+import com.dao.nbti.common.exception.ErrorCode;
+import com.dao.nbti.study.exception.NoSuchCategoryException;
 import com.dao.nbti.user.application.dto.UserCreateRequest;
 import com.dao.nbti.user.application.service.UserService;
+import com.dao.nbti.user.exception.UserException;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/user")
 public class UserController {
     private final UserService userService;
+    @Operation(summary = "회원가입", description = "사용자는 사용자 정보를 입력하여 회원가입 할 수 있다.")
     @GetMapping("/signup")
     public ResponseEntity<ApiResponse<Void>> signup(@RequestBody UserCreateRequest userCreateRequest){
         userService.createUser(userCreateRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(null));
     }
+
+    @Operation(summary = "회원 탈퇴", description = "회원 탈퇴 시 데이터 익명화 및 계정 비활성화 처리한다.")
+    @DeleteMapping("/withdraw")
+    public ResponseEntity<ApiResponse<Void>> withdraw(@AuthenticationPrincipal User user){
+        Integer userId = Integer.parseInt(user.getUsername());
+        userService.deleteUser(userId);
+        return ResponseEntity.ok(ApiResponse.success(null));
+    }
+
+    @ExceptionHandler(UserException.class)
+    public ResponseEntity<ApiResponse<Void>> handleUserException(UserException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        return ResponseEntity
+                .status(errorCode.getHttpStatus())
+                .body(ApiResponse.failure(errorCode.getCode(), errorCode.getMessage()));
+    }
+
 }
