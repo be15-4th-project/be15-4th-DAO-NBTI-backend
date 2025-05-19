8c5468423e6001b4e6e35369b7947ce20f3e18b7
Taein5415 <kskk5050@naver.com>
2025-05-19 12:30:09 +0900

    [feat #51] AuthController.refreshToken 메소드 구현

--- NBTI/src/main/java/com/dao/nbti/common/auth/application/controller/AuthController.java
+++ NBTI/src/main/java/com/dao/nbti/common/auth/application/controller/AuthController.java
@@ -2,17 +2,15 @@ package com.dao.nbti.common.auth.application.controller;
 
 import com.dao.nbti.common.auth.application.dto.LoginRequest;
 import com.dao.nbti.common.auth.application.dto.LoginResponse;
+import com.dao.nbti.common.auth.application.dto.TokenResponse;
 import com.dao.nbti.common.auth.application.service.AuthService;
 import com.dao.nbti.common.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 
@@ -28,6 +26,21 @@ public class AuthController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, cookie.toString())
                 .body(ApiResponse.success(response));
+    }
+
+
+    @PostMapping("/refresh")
+    public ResponseEntity<ApiResponse<TokenResponse>> refreshToken(
+            @CookieValue(name = "refreshToken", required = false) String refreshToken
+    ) {
+        if (refreshToken == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); // refreshToken이 없으면 401 반환
+        }
+        TokenResponse tokenResponse = authService.refreshToken(refreshToken);
+        ResponseCookie cookie = createRefreshTokenCookie(tokenResponse.getRefreshToken());  // refreshToken 쿠키 생성
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, cookie.toString())
+                .body(ApiResponse.success(tokenResponse));
     }
 
     private ResponseCookie createRefreshTokenCookie(String refreshToken) {
