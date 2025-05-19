92f302048561dc04c827296aca60b8f06035455a
Taein5415 <kskk5050@naver.com>
2025-05-19 22:06:13 +0900

    [feat #41] UserService.deleteUser 메소드 구현

--- NBTI/src/main/java/com/dao/nbti/user/application/service/UserService.java
+++ NBTI/src/main/java/com/dao/nbti/user/application/service/UserService.java
@@ -4,6 +4,7 @@ package com.dao.nbti.user.application.service;
 
 import com.dao.nbti.common.exception.ErrorCode;
 import com.dao.nbti.user.application.dto.UserCreateRequest;
+import com.dao.nbti.user.domain.aggregate.IsDeleted;
 import com.dao.nbti.user.domain.aggregate.User;
 import com.dao.nbti.user.domain.repository.UserRepository;
 import com.dao.nbti.user.exception.UserException;
@@ -12,6 +13,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @Slf4j
@@ -20,13 +24,21 @@ public class UserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+
+    @Transactional
     public void createUser(UserCreateRequest userCreateRequest) {
-        log.info(userCreateRequest.toString());
         User user = modelMapper.map(userCreateRequest, User.class);
         if(userRepository.findByAccountId(user.getAccountId()).isPresent()){
             throw new UserException(ErrorCode.LOGIN_ID_ALREADY_EXISTS);
         }
-        user.setEncodedPassword(passwordEncoder.encode(userCreateRequest.getPassword()));
+        user.setPassword(passwordEncoder.encode(userCreateRequest.getPassword()));
         userRepository.save(user);
+    }
+
+    @Transactional
+    public void deleteUser(Integer userId) {
+        User user = userRepository.findByUserIdAndDeletedAtIsNull(userId)
+                .orElseThrow(() -> new UserException(ErrorCode.USER_NOT_FOUND));
+        user.delete();
     }
 }
