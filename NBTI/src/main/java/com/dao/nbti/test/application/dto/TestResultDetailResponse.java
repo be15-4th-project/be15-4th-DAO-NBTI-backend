d923d1f582db0a44a592851cfead0c6fc1050d33
meowdule <meowdule73@gmail.com>
2025-05-19 17:17:27 +0900

    [feat #14] 검사 결과 상세 조회 기능 구현

---
+++ NBTI/src/main/java/com/dao/nbti/test/application/dto/TestResultDetailResponse.java
@@ -0,0 +1,19 @@
+package com.dao.nbti.test.application.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class TestResultDetailResponse {
+    private int langComp;
+    private int generalKnowledge;
+    private int percReason;
+    private int workMemory;
+    private int procSpeed;
+    private int spatialPerception;
+    private String aiText;
+    private LocalDateTime createdAt;
+}
