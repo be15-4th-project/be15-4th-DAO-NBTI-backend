12565c3ff009fbd58a5046f95432eced886454a7
mijuckboon <91645305+mijuckboon@users.noreply.github.com>
2025-05-20 16:42:50 +0900

    [feat] 관리자 카테고리 목록 조회 기능 구현
    - 드롭다운 메뉴에 필요한 상위/하위 카테고리 목록 조회

--- NBTI/src/main/java/com/dao/nbti/problem/domain/repository/CategoryRepository.java
+++ NBTI/src/main/java/com/dao/nbti/problem/domain/repository/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.dao.nbti.problem.domain.repository;
 import com.dao.nbti.problem.domain.aggregate.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    List<Category> findByParentCategoryIdIsNullOrderByCategoryIdAsc();
+
+    List<Category> findByParentCategoryIdIsNotNullOrderByParentCategoryIdAscCategoryIdAsc();
 }
