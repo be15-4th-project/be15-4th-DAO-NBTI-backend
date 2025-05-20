ffc1addd787973e29cc438536ed8db414036e5b1
splguyjr <splguyjr@naver.com>
2025-05-20 16:30:44 +0900

    [refactor] JpaRepository에 @Repository 애노테이션 명시

--- NBTI/src/main/java/com/dao/nbti/problem/domain/repository/ProblemRepository.java
+++ NBTI/src/main/java/com/dao/nbti/problem/domain/repository/ProblemRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProblemRepository extends JpaRepository<Problem, Integer> {
 
     // 무작위 3문제 (레벨 무관)
