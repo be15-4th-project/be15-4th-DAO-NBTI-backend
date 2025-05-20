8725b9bb72f79b6a8847c2ffe4f76412684777ef
splguyjr <splguyjr@naver.com>
2025-05-20 12:44:48 +0900

    [feat #24] 학습 결과 조회 api 구현

---
+++ NBTI/src/main/java/com/dao/nbti/study/application/dto/response/StudyResultDetailResponseDto.java
@@ -0,0 +1,30 @@
+package com.dao.nbti.study.application.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class StudyResultDetailResponseDto {
+
+    private int studyId;
+    private int totalCount;
+    private int correctCount;
+    private List<StudyResultItem> results;
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    public static class StudyResultItem {
+        private int studyResultId;
+        private int problemId;
+        private boolean isCorrect;
+        private int level;
+        private String submittedAnswer;
+        private String parentCategoryName;
+    }
+}
