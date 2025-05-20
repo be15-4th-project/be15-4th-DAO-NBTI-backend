b55db88b1ee65f26145dff26d16368de7ee07408
Taein5415 <kskk5050@naver.com>
2025-05-20 11:39:39 +0900

    [feat #38] requestPasswordReset 메소드 작성

--- NBTI/src/main/java/com/dao/nbti/common/auth/application/controller/AuthController.java
+++ NBTI/src/main/java/com/dao/nbti/common/auth/application/controller/AuthController.java
@@ -1,16 +1,12 @@
 package com.dao.nbti.common.auth.application.controller;
 
-import com.dao.nbti.common.auth.application.dto.LoginRequest;
-import com.dao.nbti.common.auth.application.dto.LoginResponse;
-import com.dao.nbti.common.auth.application.dto.TokenResponse;
+import com.dao.nbti.common.auth.application.dto.*;
 import com.dao.nbti.common.auth.application.service.AuthService;
 import com.dao.nbti.common.dto.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseCookie;
-import org.springframework.http.ResponseEntity;
+import org.apache.coyote.Response;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
@@ -60,6 +56,15 @@ public class AuthController {
                 .header(HttpHeaders.SET_COOKIE, deleteCookie.toString())
                 .body(ApiResponse.success(null));
     }
+
+    @Operation(summary = "비밀번호 찾기", description = "사용자는 아이디와 이름을 입력하여 비밀번호 변경을 요청한다.")
+    @GetMapping("/find-password")
+    public ResponseEntity<ApiResponse<TokenResponse>> requestPasswordReset(@RequestBody PasswordFindRequest request){
+        TokenResponse response = authService.findPassword(request);
+
+        return ResponseEntity.ok().body(ApiResponse.success(response));
+    }
+
 
     private ResponseCookie createRefreshTokenCookie(String refreshToken) {
         return ResponseCookie.from("refreshToken", refreshToken)
