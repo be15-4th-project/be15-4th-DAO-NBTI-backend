f8e415054adaf8e3390304bac25b7f601d420885
Taein5415 <kskk5050@naver.com>
2025-05-21 00:56:30 +0900

    [feat #47] TestResultController.getTestResults 구현

--- NBTI/src/main/java/com/dao/nbti/test/application/controller/TestResultController.java
+++ NBTI/src/main/java/com/dao/nbti/test/application/controller/TestResultController.java
@@ -2,6 +2,8 @@ package com.dao.nbti.test.application.controller;
 
 import com.dao.nbti.common.dto.ApiResponse;
 import com.dao.nbti.common.dto.Pagination;
+import com.dao.nbti.test.application.dto.request.AdminTestResultSearchCondition;
+import com.dao.nbti.test.application.dto.response.AdminTestResultSummaryResponse;
 import com.dao.nbti.test.application.dto.response.TestResultDetailResponse;
 import com.dao.nbti.test.application.dto.request.TestResultSearchCondition;
 import com.dao.nbti.test.application.dto.response.TestResultSummaryResponse;
@@ -71,5 +73,35 @@ public class TestResultController {
     ) {
         TestResultDetailResponse detail = testResultService.getTestResultDetail(testResultId, userId);
         return ResponseEntity.ok(ApiResponse.success(detail));
+    }
+
+    @Operation(summary = "검사 목록 조회", description = "검사 결과 ID를 통해 상세 정보를 조회합니다.")
+    @GetMapping("/list/admin")
+    public ResponseEntity<ApiResponse<Map<String,Object>>> getTestResults(
+            @Parameter(description = "사용자 id", example="user01")
+            @RequestParam(required = false) String accountId,
+
+            @Parameter(description = "조회할 연도", example = "2025")
+            @RequestParam(required = false) Integer year,
+
+            @Parameter(description = "조회할 월 (1~12)", example = "5")
+            @RequestParam(required = false) Integer month,
+
+            @Parameter(description = "페이지 및 정렬 정보")
+            @PageableDefault(size = 10, sort = "createdAt") Pageable pageable
+    ) {
+        AdminTestResultSearchCondition condition = new AdminTestResultSearchCondition(year, month, accountId);
+
+        AdminTestResultSummaryResponse resultPage = testResultService.getAdminTestResultList(condition, pageable);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("content", resultPage.getPage().getContent());
+        response.put("pagination", Pagination.builder()
+                .currentPage(resultPage.getPage().getNumber() + 1)
+                .totalPage(resultPage.getPage().getTotalPages())
+                .totalItems(resultPage.getPage().getTotalElements())
+                .build());
+
+        return ResponseEntity.ok(ApiResponse.success(response));
     }
 }
