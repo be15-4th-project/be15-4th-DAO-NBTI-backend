9917c53996607911cff979ccc6220fcb0e180962
Taein5415 <kskk5050@naver.com>
2025-05-20 22:17:52 +0900

    [feat #43] UserService.getUserList 메소드 구현

--- NBTI/src/main/java/com/dao/nbti/user/application/service/UserService.java
+++ NBTI/src/main/java/com/dao/nbti/user/application/service/UserService.java
@@ -3,18 +3,22 @@ package com.dao.nbti.user.application.service;
 
 
 import com.dao.nbti.common.exception.ErrorCode;
-import com.dao.nbti.user.application.dto.IdDuplicateResponse;
-import com.dao.nbti.user.application.dto.UserCreateRequest;
-import com.dao.nbti.user.application.dto.UserInfoResponse;
+import com.dao.nbti.user.application.dto.*;
 import com.dao.nbti.user.domain.aggregate.User;
 import com.dao.nbti.user.domain.repository.UserRepository;
+import com.dao.nbti.user.domain.repository.UserRepositoryCustom;
 import com.dao.nbti.user.exception.UserException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -23,6 +27,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final ModelMapper modelMapper;
     private final PasswordEncoder passwordEncoder;
+    private final UserRepositoryCustom userRepositoryCustom;
 
     @Transactional
     public void createUser(UserCreateRequest userCreateRequest) {
@@ -58,5 +63,21 @@ public class UserService {
 
         UserInfoResponse response = modelMapper.map(user, UserInfoResponse.class);
         return response;
+    }
+
+    public UserAdminViewResponse getUserList(UserSearchCondition condition, Pageable pageable) {
+        List<User> users = userRepositoryCustom.getUsers(
+                condition,
+                pageable
+        );
+
+        List<UserAdminViewDTO> content = users.stream()
+                .map(user -> modelMapper.map(user, UserAdminViewDTO.class))
+                .collect(Collectors.toList());
+
+        long total = userRepositoryCustom.countUsers(condition); // 조건에 맞는 전체 유저 수
+
+        return new UserAdminViewResponse(new PageImpl<>(content, pageable, total));
+
     }
 }
