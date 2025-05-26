38bcb6d6367b368bb5e5a0a02a8e223202288567
splguyjr <splguyjr@naver.com>
2025-05-26 15:09:04 +0900

    [chore] backend container 제거, 불필요한 test contextLoad문 제거

--- NBTI/src/test/java/com/dao/nbti/NbtiApplicationTests.java
+++ NBTI/src/test/java/com/dao/nbti/NbtiApplicationTests.java
@@ -2,12 +2,9 @@ package com.dao.nbti;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 class NbtiApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
